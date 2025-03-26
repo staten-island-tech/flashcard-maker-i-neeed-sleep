e634857522bd2535cd85a1164a425e4422a65fd5
i-neeed-sleep <kammyl@nycstudents.net>
2025-03-26 09:25:14 -0400

    eeeee

--- .py
+++ .py
@@ -1,14 +1,19 @@
 import json
 import random
 
+try:
+    with open("FlashCards.json", "r") as file:
+        cd = json.load(file)
+except FileNotFoundError:
+    cd = []
 
+m = input("teacher or student mode?").lower()
 
-
-m = ""
-p = input()
-
-
+class t:
+    def __innit__(w, d):
+        word = w
+        definition = d
 
 
 with open("FlashCards.json", 'w') as file:
-    json.dump()
+    json.dump(cd, file, indent = 4)
