ee0e2509099e821f4f074096314a689a9a194359
i-neeed-sleep <kammyl@nycstudents.net>
2025-04-22 09:02:40 -0400

    eeee

--- .py
+++ .py
@@ -1,14 +1,17 @@
 import json
 import random
 
+aaaa = open("./FlashCard.json", encoding="utf8")
+fc = json.load(aaaa)
+pfc = fc
+
 try:
     with open("FlashCards.json", "r") as file:
         fc = json.load(file)
 except:
     fc = []
 
-
-m = "student"
+m = input("teacher or student mode?")
 s = 0
 a = ""
 
@@ -18,21 +21,20 @@ if "teacher" in m:
         p = p.split(":")
         fc.append({"w":p[0],"d":p[1]})
         p = input("next ")
+    fc += pfc
+    with open("FlashCard.json", "w") as file:
+        json.dump(fc, file, indent=2)
 
 if "student" in m:
-    aaaa = open("./FlashCard.json", encoding="utf8")
-    fc = json.load(aaaa)
-    while ";" in a == False:
-        x = random.choice(fc)
+    while a != ";":
+        x = random.choice(pfc)
         a = input(f"{x["w"]}(input ; to stop)")
-        if a in x["d"]:
-            print("Correct. ")
-            s += 1
-        else:
-            s = 0
-            print("Incorrect. ")
-        if s%5 == 0:
-            print(f"You have a streak of {s}")
-
-with open("FlashCard.json", "w") as file:
-     json.dump(fc, file, indent=2)
+        if a != ";":
+            if a == x["d"]:
+                print("Correct. ")
+                s += 1
+            else:
+                s = 0
+                print("Incorrect. ")
+            if s%5 == 0:
+                print(f"You have a streak of {s}")
